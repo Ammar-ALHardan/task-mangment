4aa0a39224e80102b4fe9398beff65aeaf93bef1
Ammar-ALHardan <ammarhardan863@gmail.com>
2024-05-26 09:00:25 +0300

    create event with signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component , signal , computed } from '@angular/core';
+import { Component , signal , computed, Input , input, Output, EventEmitter, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -11,8 +11,27 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string ;
+  @Input({required: true}) name!: string ;
+  @Output() select = new EventEmitter();
+  // select = output<string>();
+  // avatar = input.required<string>(); 
+  // name = input.required<string>(); 
+  // imagePath = computed( () => {
+  //   'assets/users/' + this.avatar()
+  // } );
+
+    get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+
+
+
   // selectedUser = signal(DUMMY_USERS[randomIndex]) ;
-  // imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar);
 
   // get imagePath() {
   //   return 'assets/users/' + this.selectedUser().avatar

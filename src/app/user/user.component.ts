16ed444b28368da30633318995095c627d200768
Ammar-ALHardan <ammarhardan863@gmail.com>
2024-05-26 11:36:35 +0300

    new component of task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,12 @@
 import { Component , signal , computed, Input , input, Output, EventEmitter, output } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
@@ -11,25 +18,27 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string ;
-  @Input({required: true}) name!: string ;
-  @Output() select = new EventEmitter();
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string ;
+  // @Input({required: true}) name!: string ;
+
+  @Input({required: true}) user!: User;
+  @Output() select = new EventEmitter<string>();
+
+
+    get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+
   // select = output<string>();
   // avatar = input.required<string>(); 
   // name = input.required<string>(); 
   // imagePath = computed( () => {
   //   'assets/users/' + this.avatar()
   // } );
-
-    get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
-  onSelectUser() {
-    this.select.emit(this.id);
-  }
-
-
 
   // selectedUser = signal(DUMMY_USERS[randomIndex]) ;
 
